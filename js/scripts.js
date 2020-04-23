31d44658af2bdbbc67ac25204eea719f366b42a6
paulmburu08 <paulmburu08@gmail.com>
2020-04-23 10:44:32 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,9 @@
-var slideIndex = 0;
-showSlides();
+$("document").ready(function(){
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
+  $("#mc-embedded-subscribe").click(function(){
+    $("#approvalDenial").show();
+    $("#mc_embed_signup").hide();
+  });
+
+});
+
